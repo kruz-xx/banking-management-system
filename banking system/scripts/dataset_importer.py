652e936f6e222963252a827ca0c837b4f63683b5
Krupa Sodagar <krupavsodagar@gmail.com>
2026-08-05 12:44:55 +0530

    basic outline in dataset_importer

--- banking system/scripts/dataset_importer.py
+++ banking system/scripts/dataset_importer.py
@@ -2,21 +2,32 @@ from pathlib import Path
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+INPUT_FILE = BASE_DIR / "data" / "raw-data.xlsx"
+OUTPUT_DIR = BASE_DIR / "data" / "processed-data"
 
-INPUT_FILE = BASE_DIR / "data" / "raw_data.xlsx"
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 df = pd.read_excel(INPUT_FILE)
 
-print(f"Loaded {len(df)} records.")
+customers=[]
+accounts=[]
+transactions=[]
 
-print("\nAccount Types:")
-print(df["Account Type"].unique())
+customer_id_map={}
 
-print("\nTransaction Types:")
-print(df["Transaction Type"].unique())
+customer_counter=1
+transaction_counter=1
 
-print("\nCurrencies:")
-print(df["Currency"].unique())
+for _, row in df.iterrows():
+    account_id = row['Account ID']
+    customer_name = row['Customer Name']
+    account_type = row['Account Type']
+    branch = row['Branch']
+    transaction_type = row['Transaction Type']
+    transaction_amount = row['Transaction Amount']
+    balance = row['Balance']
+    currency = row['Currency']
+    
+    
 
-print("\nBranches:")
-print(df["Branch"].unique())
+
